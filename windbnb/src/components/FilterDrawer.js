e3e3c414ea0807c7a17f249b695507dacd3c10de
issam <aissam.16.25@gmail.com>
2023-01-11 23:45:04 +0100

    adjust FilterDrawer layout for desktop screen

--- windbnb/src/components/FilterDrawer.js
+++ windbnb/src/components/FilterDrawer.js
@@ -6,14 +6,16 @@ import GuestsFilter from "./GuestsFilter"
 const FilterDrawer = () => {
   return (
     <div className='absolute w-full h-[70vh] bg-white left-0 -top-4 pt-6 '>
-      <div className='flex items-center justify-between container gap-4 shadow-sm rounded-2xl'>
-          <Input type='location'/>
-          <Input type='guests'/>
+      <div className='flex items-center justify-between container shadow-sm rounded-2xl'>
+        <div className='basis-4/5 flex items-center'>
+          <Input type='location' className="basis-1/2"/>
+          <Input type='guests' className="basis-1/2 "/>
+        </div>
           <Button className="hidden md:flex"/>
       </div>
       <div className='container flex items-center  gap-4 mt-10'>
-        <LocationFilterResults className="basis-1/2"/>
-        <GuestsFilter className="basis-1/3"/>
+        <LocationFilterResults className="basis-2/5"/>
+        <GuestsFilter className="basis-2/5"/>
         {/* <Button className=""/> */}
         
       </div>
--- windbnb/src/components/Input.js
+++ windbnb/src/components/Input.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Input = ({className,type,}) => {
   return (
-    <div className='relative flex-1 font-muli'>
+    <div className={`${className} relative font-muli`}>
       <span className='absolute left-6 top-2 uppercase text-[9px] font-bold text-w-black'>{type ==="guests" ?"guests": 'location'}</span>
       <input type="text"  placeholder={`Add ${type ==="guests" ?"guests": 'location'}`} className={`${className} placeholder:font-normal placeholder:text-sm pt-6  text-w-black text-sm p-4 px-6 rounded-2xl lg:w-full focus:border focus:border-w-gray focus:outline-none`}/>
     </div>
