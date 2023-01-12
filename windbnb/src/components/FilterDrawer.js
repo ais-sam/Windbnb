bcf5e4cf4ab79c2b224b48954fb820230bd00023
issam <aissam.16.25@gmail.com>
2023-01-12 22:19:25 +0100

    improve FilterDrawer layout

--- windbnb/src/components/FilterDrawer.js
+++ windbnb/src/components/FilterDrawer.js
@@ -5,19 +5,18 @@ import LocationFilterResults from './LocationFilterResults'
 import GuestsFilter from "./GuestsFilter"
 const FilterDrawer = () => {
   return (
-    <div className='absolute w-full h-[92vh] sm:h-[70vh]  left-0 -top-4 pt-6 pb-11 bg-slate-500 '>
-      <div className='flex items-center justify-between container shadow-sm rounded-2xl'>
-        <div className='basis-4/5 flex flex-col sm:flex-row sm:items-center'>
-          <Input type='location' className="w-full sm:basis-1/2"/>
-          <Input type='guests' className="w-full sm:basis-1/2 "/>
+    <div className='absolute w-full h-[93vh] md:h-[70vh]  left-0 -top-4 pt-6 pb-11 bg-white'>
+      <div className='flex items-center justify-between container shadow-sm rounded-2xl px-2'>
+        <div className='basis-full md:basis-4/5 md:flex  md:flex-row md:items-center '>
+          <Input type='location' className="w-full md:basis-1/2 mb-1 md:mb-0"/>
+          <Input type='guests' className="w-full md:basis-1/2 "/>
         </div>
-          <Button className="hidden sm:flex"/>
+          <Button className="hidden md:flex"/>
       </div>
-      <div className='container flex flex-col sm:flex-row  md:items-center gap-4 mt-10 pl-6 sm:pl-0'>
+      <div className='container flex flex-col md:flex-row  md:items-center gap-4 mt-10 pl-6 md:pl-0'>
         <LocationFilterResults className="basis-2/5"/>
         <GuestsFilter className="basis-2/5"/>
-        <Button className="flex sm:hidden sel self-center w-fit mb-8"/>
-        
+        <Button className="flex md:hidden sel self-center w-fit mb-8"/>
       </div>
 
     </div>
