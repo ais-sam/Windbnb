ddaf2e835224ed1e0d3ef46d2a1054cae897eb4d
issam <aissam.16.25@gmail.com>
2023-01-12 22:18:37 +0100

    add location icon

--- windbnb/src/components/LocationFilterResults.js
+++ windbnb/src/components/LocationFilterResults.js
@@ -3,8 +3,8 @@ import React from 'react'
 const LocationFilterResults = ({className}) => {
   const cities =["Helsinki, Finland","Turku, Finland","Oulu, Finland","Vaasa, Finland"]
   return (
-    <ul className={`${className} font-muli text-w-gray sm:pl-4`}>
-        {cities.map((city,index)=><li key={index} className="mb-4">{city}</li>)}
+    <ul className={`${className} font-muli text-w-gray md:pl-4`}>
+        {cities.map((city,index)=><li key={index} className="mb-4"><i class="fas fa-map-marker-alt"></i> {city}</li>)}
     </ul>
   )
 }
