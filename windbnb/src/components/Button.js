217642530d9fdbee26f612d4b675bed30e4c7dc2
issam <aissam.16.25@gmail.com>
2023-01-13 23:11:10 +0100

    Edit Search button

--- windbnb/src/components/Button.js
+++ windbnb/src/components/Button.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import search from '../assets/search.png'
+import searchWhite from '../assets/search-white.png'
 
 
 const Button = ({className}) => {
   return (
+    <>
     <button
-      className={`${className} bg-w-gray p-6 py-4 rounded-2xl text-white text-sm flex items-center gap-3`}
+      className={`${className} bg-w-pink p-6 py-[14px] rounded-2xl text-white text-sm flex items-center gap-3`}
     >
-      <img src={search} className="w-5" alt="" />
+      <img src={searchWhite} className="w-6" alt="" />
       Search
     </button>
+    </>
   );
 }
 
