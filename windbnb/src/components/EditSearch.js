608b4b36b363f4278984c18dc7383cdd8e914816
issam <aissam.16.25@gmail.com>
2023-01-13 23:30:55 +0100

    Add exit icon

--- windbnb/src/components/EditSearch.js
+++ windbnb/src/components/EditSearch.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import exit from "../assets/exit.png"
 
 const EditSearch = () => {
   return (
     <div className="flex items-center justify-between py-1 md:hidden font-muli px-4 sm:pl-8">
       <p className="text-sm font-bold">Edit your search</p>
-      <i class="fas fa-times text-xl"></i>
+      <img src={exit} alt="" className='w-6'/>
     </div>
   );
 }
