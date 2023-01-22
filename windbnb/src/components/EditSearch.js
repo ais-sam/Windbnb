27b7e5218fbf401dedb98d6560d33514f811fd8a
issam <aissam.16.25@gmail.com>
2023-01-22 23:20:56 +0100

    Close modal by clicking on the X button

--- windbnb/src/components/EditSearch.js
+++ windbnb/src/components/EditSearch.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import exit from "../assets/exit.png"
+import useGlobal from '../contexts/GlobalContext';
 
 const EditSearch = () => {
+  const {setModal} = useGlobal()
+
   return (
     <div className="mb-2 flex items-center justify-between py-1 md:hidden font-muli px-4 sm:pl-8">
       <p className="text-sm font-bold">Edit your search</p>
-      <img src={exit} alt="" className='w-6'/>
+      <button onClick={()=>setModal(false)}><img src={exit} alt="" className='w-6'/></button>
     </div>
   );
 }
