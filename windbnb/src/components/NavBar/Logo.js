e943eeb6356452081e39dd79b2cbc7a659726c56
issam <aissam.16.25@gmail.com>
2023-01-24 22:26:05 +0100

    Update states after clicking the Logo

--- windbnb/src/components/NavBar/Logo.js
+++ windbnb/src/components/NavBar/Logo.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import logo from "../../assets/logo.svg"
+import useGlobal from '../../contexts/GlobalContext'
 
 const Logo = ({className}) => {
+  const {setChoosedLocation,setTotalGuests,setGuests,setLocation} = useGlobal()
+  
+  const handleLogoClick = ()=>{
+    setChoosedLocation('')
+    setTotalGuests(0)
+    setLocation('')
+    setGuests(0)
+  }
   return (
-    <div className={className} >
+    <button className={className} onClick={handleLogoClick}>
       <img src={logo} alt=""/>
-    </div>
+    </button>
   )
 }
 
