fb939873d7e3723e5808986a1b837b93ef029d46
issam <aissam.16.25@gmail.com>
2023-01-24 21:51:25 +0100

    Add Title Component

--- windbnb/src/components/CardsGrid.js
+++ windbnb/src/components/CardsGrid.js
@@ -2,6 +2,7 @@ import React from 'react'
 import stays from "../utils/stays.json"
 import Card from "../components/Card"
 import useGlobal from '../contexts/GlobalContext'
+import Title from './Title'
 
 const CardsGrid = () => {
   const {choosedLocation,totalGeusts} = useGlobal()
@@ -24,12 +25,7 @@ const CardsGrid = () => {
   
   return (
     <div className="mt-6">
-      <div className="font-montserrat flex items-center justify-between">
-        <h1 className='text-lg font-bold '>Stays in Finland</h1>
-        <div className='text-sm'>
-          <span>{numberOfStays}</span> stays
-        </div>
-      </div>
+      <Title stays ={numberOfStays}/>
       <div className='mt-6 grid md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-8'>
           {filtredResults.map((el,index)=>{
             return <Card key={index} stay={el}/>
--- windbnb/src/components/Title.js
+++ windbnb/src/components/Title.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Title = () => {
+const Title = ({stays}) => {
+
   return (
-    <div>Title</div>
+    <div className="font-montserrat flex items-center justify-between">
+    <h1 className='text-lg font-bold '>Stays in Finland</h1>
+    <div className='text-sm'>
+      <span>{stays}</span> stays
+    </div>
+  </div>
   )
 }
 
