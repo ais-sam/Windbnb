a53cbf4f9fb1bce59eb31264179d533a41756599
issam <aissam.16.25@gmail.com>
2023-01-25 12:21:45 +0100

    Fix the Global Provider issue

--- windbnb/src/App.js
+++ windbnb/src/App.js
@@ -1,47 +1,15 @@
-import { useState } from 'react';
-import './App.css';
-import { GlobalContext, GlobalProvider } from './contexts/GlobalContext';
+import { GlobalProvider } from './contexts/GlobalContext';
 import Home from "./pages/Home";
 
 
 function App() {
-  // show / hide modal
-  const [isModal,setModal] = useState(false)
-  // show / hide cities search results
-  const [isCities,setCities] = useState(false)
-  // show / hide guest filters
-  const [isGuestFilters,setGuestFilters] = useState(false)
-  // the choosed location
-  const [choosedLocation,setChoosedLocation] = useState('Helsinki, Finland')
-  // total number of guests
-  const [totalGeusts, setTotalGuests] = useState(0)
-  // location
-  const [location, setLocation] = useState('');
-  //  number of guest
-  const [guests, setGuests] = useState(0);
-  // number of adults
-  const [numberOfAdult,setNumberOfAdult] = useState(0)
-  // number of children
-  const [numberOfChildren,setNumberOfChildren] = useState(0)
   
-  const value = {
-    isModal,setModal,
-    isCities,setCities,
-    isGuestFilters,setGuestFilters,
-    choosedLocation,setChoosedLocation,
-    location, setLocation,
-    guests, setGuests,
-    numberOfAdult,setNumberOfAdult,
-    numberOfChildren,setNumberOfChildren,
-    totalGeusts, setTotalGuests
-
-  }
   return (
-    <GlobalContext.Provider value={value}>
+    <GlobalProvider>
       <div className="relative" >
         <Home/>
       </div>
-    </GlobalContext.Provider>
+    </GlobalProvider>
   );
 }
 
--- windbnb/src/contexts/GlobalContext.js
+++ windbnb/src/contexts/GlobalContext.js
@@ -6,27 +6,42 @@ import { createContext, useContext,useState } from "react";
 export const GlobalContext = createContext()
 
 // create context provider
-export const GlobalProvider = ({childern}) => {
+export const GlobalProvider = ({children}) => {
+  // show / hide modal
   const [isModal,setModal] = useState(false)
+  // show / hide cities search results
   const [isCities,setCities] = useState(false)
+  // show / hide guest filters
   const [isGuestFilters,setGuestFilters] = useState(false)
-  const [choosedLocation,setChoosedLocation] = useState('')
-  const [choosedNumberOfGuests,setChoosedNumberOfGuests] = useState(0)
-  // const [location, setLocation] = useState('');
-  // const [guests, setGuests] = useState(0);
+  // the choosed location
+  const [choosedLocation,setChoosedLocation] = useState('Helsinki, Finland')
+  // total number of guests
+  const [totalGeusts, setTotalGuests] = useState(0)
+  // location
+  const [location, setLocation] = useState('');
+  //  number of guest
+  const [guests, setGuests] = useState(0);
+  // number of adults
+  const [numberOfAdult,setNumberOfAdult] = useState(0)
+  // number of children
+  const [numberOfChildren,setNumberOfChildren] = useState(0)
+  
   const value = {
     isModal,setModal,
     isCities,setCities,
     isGuestFilters,setGuestFilters,
     choosedLocation,setChoosedLocation,
-    
-  //   location, setLocation,
-  //   guests, setGuests
-   }
+    location, setLocation,
+    guests, setGuests,
+    numberOfAdult,setNumberOfAdult,
+    numberOfChildren,setNumberOfChildren,
+    totalGeusts, setTotalGuests
+
+  }
 
   return (
     <GlobalContext.Provider value={value}>
-        {childern}
+        {children}
     </GlobalContext.Provider>
   )
 }
