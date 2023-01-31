9322ae8673828b51b7a36352884674a37faa95f0
issam <aissam.16.25@gmail.com>
2023-01-31 15:31:37 +0100

    make the title dynamic

--- windbnb/src/components/Main/Title.js
+++ windbnb/src/components/Main/Title.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import useGlobal from '../../contexts/GlobalContext'
+
 
 const Title = ({stays}) => {
-
+  const {state} = useGlobal()
+  const country =state.choosedLocation? state.choosedLocation.split(',')[1].trim(): "Finland"
   return (
     <div className="font-montserrat flex items-center justify-between">
-    <h1 className='text-lg font-bold '>Stays in Finland</h1>
+    <h1 className='text-lg font-bold '>Stays in {country}</h1>
     <div className='text-sm'>
       <span>{stays}</span> stays
     </div>
